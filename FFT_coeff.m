f578cb00d479d4894906850fd855f765b2641167
allenyang <aryang@caltech.edu>
2024-01-27 16:59:02 -0800

    Changes FFT implementation to match Leibovici

--- FFT_coeff.m
+++ FFT_coeff.m
@@ -1,4 +1,4 @@
-function [C_n] = FFT_coeff(phi_D, N)
+function [C_n] = FFT_coeff(phi_D)
 % Computes coefficients of Fourier Series from discretized grid using FFT
 % Assumes phi_D is value of some continuous function on x = 0 to x = 1
 % over discrete x mesh.
@@ -6,12 +6,6 @@ function [C_n] = FFT_coeff(phi_D, N)
 % phi_D should be a column vector
 
 % Calculates DFT off phi_D, assuming x mesh from x = 0 to x = 1
-FFT_phi = fft(phi_D) ./ length(phi_D);
-
-% Trims coefficients for N terms
-FFT_phi = FFT_phi(1:N);
-
-% C_n = conjugate(C_{-n})
-C_n = [flip(conj(FFT_phi)); FFT_phi(2:end)];
+C_n = fft(phi_D) ./ length(phi_D);
 end
 
--- FFT_compute_func.m
+++ FFT_compute_func.m
@@ -1,8 +1,13 @@
 function [FS_func] = FFT_compute_func(C_n)
 % Returns function that computes fourier series with dependency on x mesh
 % column vector
-N = (length(C_n) + 1) / 2;
-
-FS_func =  @(x) real(exp(2i.*pi.*x*(-N+1:N-1)) * C_n);
+%N = (length(C_n) + 1) / 2;
+N = length(C_n);
+if mod(N, 2) == 0
+    k = [0:floor(N/2), -floor(N/2)+1:-1];
+else
+    k = [0:floor(N/2), -floor(N/2):-1];
+end
+FS_func = @(x) real(exp(2i*pi*x*k) * C_n);
 end
 
