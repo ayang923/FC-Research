f0d0cf853236252846584894e92528961d2738df
allenyang <aryang@caltech.edu>
2024-02-01 20:53:42 -0800

    Changed fft compute function and performed analysis without inteprolation

--- FFT_compute_func.m
+++ FFT_compute_func.m
@@ -1,13 +1,15 @@
-function [FS_func] = FFT_compute_func(C_n)
+function [phi_FT] = FFT_compute_func(C_n, D)
 % Returns function that computes fourier series with dependency on x mesh
 % column vector
 %N = (length(C_n) + 1) / 2;
 N = length(C_n);
 if mod(N, 2) == 0
-    k = [0:floor(N/2), -floor(N/2)+1:-1];
+    %k = [0:floor(N/2), -floor(N/2)+1:-1];
+     freq = [0:N/2, -N/2+1:-1];
 else
-    k = [0:floor(N/2), -floor(N/2):-1];
+    %k = [0:floor(N/2), -floor(N/2):-1];
+   freq = [0:(N-1)/2, -(N-1)/2:-1];
 end
-FS_func = @(x) real(exp(2i*pi*x*k) * C_n);
+phi_FT = real(exp(2i*pi*D*freq) * C_n);
 end
 
