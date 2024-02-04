da950d6661c413ba5ab4d0eda8b7449cc3862cbb
allenyang <aryang@caltech.edu>
2024-02-04 14:33:57 -0800

    Error analysis for 1D-FC
    doesn't look quite right

--- FFT_compute_func.m
+++ FFT_compute_func.m
@@ -1,7 +1,6 @@
 function [phi_FT] = FFT_compute_func(C_n, L, D)
 % Returns function that computes fourier series with dependency on x mesh
 % column vector
-%N = (length(C_n) + 1) / 2;
 N = length(C_n);
 if mod(N, 2) == 0
      freq = [0:N/2, -N/2+1:-1];
