623a0c92594bbaabfe98f6b67ac05175d4073efa
allenyang <aryang@caltech.edu>
2024-02-10 17:29:50 -0800

    IMplements C2 and S type patch solvers with gram interpolation

---
+++ gram_interpolation/FFT_compute_func.m
@@ -0,0 +1,12 @@
+function [phi_FT] = FFT_compute_func(C_n, L, D)
+% Returns function that computes fourier series with dependency on x mesh
+% column vector
+N = length(C_n);
+if mod(N, 2) == 0
+     freq = [0:N/2, -N/2+1:-1];
+else
+   freq = [0:(N-1)/2, -(N-1)/2:-1];
+end
+phi_FT = real(exp(2i*pi*D*freq/L) * C_n);
+end
+
