f3d678a25cf7e20511e461ccddd08ab9f7b7c120
allenyang <aryang@caltech.edu>
2025-08-30 16:58:33 -0400

    IE_curve_seq construct_A_b and u_num working

--- examples/poisson-examples/IE_curve_obj.m
+++ examples/poisson-examples/IE_curve_obj.m
@@ -9,20 +9,15 @@ classdef IE_curve_obj < handle
         l_1_dprime
         l_2_dprime
         
-        n
-        
         w
         w_prime
-        
-        s_mesh
-        theta_mesh
         
         curve_idx
         next_curve
     end
     
     methods
-        function obj = IE_curve_obj(curve, curve_idx, cfac, p)
+        function obj = IE_curve_obj(curve, curve_idx, p)
             %IE_CURVE_OBJ Construct an instance of this class
             %   Detailed explanation goes here
             obj.l_1 = curve.l_1;
@@ -31,25 +26,72 @@ classdef IE_curve_obj < handle
             obj.l_2_prime = curve.l_2_prime;
             obj.l_1_dprime = curve.l_1_dprime;
             obj.l_2_dprime = curve.l_2_dprime;
-            
-            obj.n = ceil((curve.n-1)*cfac);
-            
+                        
             obj.curve_idx = curve_idx;
-            curve_idx
             
             v = @(s) (1/p-1/2)*(1-2*s).^3+1/p*(2*s-1)+1/2;
             v_prime = @(s) 2/p-6*(1/p-1/2)*(1-2*s).^2;
 
             obj.w = @(s) v(s).^p./(v(s).^p+v(1-s).^p);
             obj.w_prime = @(s) p*((v_prime(s).*v(s).^(p-1))./(v(s).^p+v(1-s).^p)-(v(s).^(p-1).*v_prime(s)-v(1-s).^(p-1).*v_prime(1-s)).*v(s).^p./(v(s).^p+v(1-s).^p).^2);
-            
-            ds = 1/obj.n;
-            obj.s_mesh = linspace(0, 1-ds, obj.n);
-            
-            obj.theta_mesh = obj.w(obj.s_mesh);
         end
         
+        function [s_mesh, ds] = s_mesh(obj, n)
+            ds = 1/n;
+            s_mesh = linspace(0, 1-ds, n)';
+        end
+        
+        function theta_mesh = theta_mesh(obj, n)
+            [s_mesh, ~] = obj.s_mesh(n);
+            theta_mesh = obj.w(s_mesh);
+        end
+        
+        function u_num_curve = u_num_curve(obj, x, y, curve_param, gr_phi)
+            curve_n = curve_param.curve_n(obj.curve_idx);
+            start_idx = curve_param.start_idx(obj.curve_idx);
+            end_idx = curve_param.end_idx(obj.curve_idx);
+            [s_mesh, ds] =  obj.s_mesh(curve_n);
+            theta_mesh = obj.w(s_mesh);
+
+            u_num_curve = transpose(obj.K_general([x; y], theta_mesh).*gr_phi(start_idx:end_idx).*sqrt(obj.l_1_prime(theta_mesh).^2+obj.l_2_prime(theta_mesh).^2)) * ones(curve_n, 1) * ds;
+        end
+        
+        function K_general_eval = K_general(obj, x, theta)
+            K_general_eval = -1 / (2 * pi) * ( ...
+                (x(1) - obj.l_1(theta)) .* obj.l_2_prime(theta) - ...
+                (x(2) - obj.l_2(theta)) .* obj.l_1_prime(theta) ...
+            ) ./ ( ...
+                sqrt(obj.l_1_prime(theta).^2 + obj.l_2_prime(theta).^2) .* ...
+                ( ...
+                    (x(1) - obj.l_1(theta)).^2 + ...
+                    (x(2) - obj.l_2(theta)).^2 ...
+                ) ...
+            );
+        end
+        
+        function  K_boundary_same_point_eval = K_boundary_same_point(obj, theta)
+            K_boundary_same_point_eval = -1 / (4 * pi) * ( ...
+                obj.l_2_prime(theta) .* obj.l_1_dprime(theta) - ...
+                obj.l_1_prime(theta) .* obj.l_2_dprime(theta) ...
+            ) ./ ( ...
+                (obj.l_1_prime(theta).^2 + obj.l_2_prime(theta).^2).^(3/2) ...
+            );
+        end
+        
+        function K_boundary_eval = K_boundary(obj, theta_1, theta_2, curve_2)
+            K_boundary_eval = -1 / (2 * pi) * ( ...
+                (obj.l_1(theta_1) - curve_2.l_1(theta_2)) .* curve_2.l_2_prime(theta_2) - ...
+                (obj.l_2(theta_1) - curve_2.l_2(theta_2)) .* curve_2.l_1_prime(theta_2) ...
+            ) ./ ( ...
+                sqrt(curve_2.l_1_prime(theta_2).^2 + curve_2.l_2_prime(theta_2).^2) .* ...
+                ( ...
+                    (obj.l_1(theta_1) - curve_2.l_1(theta_2)).^2 + ...
+                    (obj.l_2(theta_1) - curve_2.l_2(theta_2)).^2 ...
+                ) ...
+            );
+        end
     end
+
 end
 
 
--- examples/poisson-examples/IE_curve_seq_obj.m
+++ examples/poisson-examples/IE_curve_seq_obj.m
@@ -8,23 +8,69 @@ classdef IE_curve_seq_obj < handle
     end
     
     methods
-        function obj = IE_curve_seq_obj(curve_seq, cfac, p)
+        function obj = IE_curve_seq_obj(curve_seq, p)
             %IE_CURVE_SEQ_OBJ Construct an instance of this class
             
             obj.n_curves = curve_seq.n_curves;
-            obj.first_curve = IE_curve_obj(curve_seq.first_curve, 1, cfac, p);
+            obj.first_curve = IE_curve_obj(curve_seq.first_curve, 1, p);
             
             curr_curve = curve_seq.first_curve;
             curr_ie_curve = obj.first_curve;
             for i = 2:obj.n_curves
                 curr_curve= curr_curve.next_curve;
-                curr_ie_curve.next_curve = IE_curve_obj(curr_curve, i, cfac, p);
+                curr_ie_curve.next_curve = IE_curve_obj(curr_curve, i, p);
                 curr_ie_curve = curr_ie_curve.next_curve;
             end
             obj.last_curve = curr_ie_curve;
             curr_ie_curve.next_curve = obj.first_curve;
         end
         
+        function [A, b] = construct_A_b(obj, curve_param, f)
+            curve_n = curve_param.curve_n;
+            n_total = curve_param.n_total;
+            start_idx = curve_param. start_idx;
+            end_idx = curve_param.end_idx;
+            
+            A = zeros(n_total, n_total);
+            b = zeros(n_total, 1);
+
+
+            curr_y = obj.first_curve;
+            for i = 1:obj.n_curves
+                [s_mesh_y, ds_y] = curr_y.s_mesh(curve_n(i));
+                theta_mesh_y = curr_y.w(s_mesh_y);
+                b(start_idx(i):end_idx(i)) = curr_y.w_prime(s_mesh_y).*f(curr_y.l_1(theta_mesh_y), curr_y.l_2(theta_mesh_y));
+
+                curr_x = obj.first_curve;
+                for j = 1:obj.n_curves
+                    [s_mesh_x, ds_x] = curr_x.s_mesh(curve_n(j));
+                    theta_mesh_x = curr_x.w(s_mesh_x);
+                    [theta_2, theta_1] = meshgrid(theta_mesh_y, theta_mesh_x);
+
+                    A_local = curr_x.w_prime(s_mesh_x).*curr_x.K_boundary(theta_1, theta_2, curr_y).*sqrt(curr_y.l_1_prime(theta_mesh_y').^2+curr_y.l_2_prime(theta_mesh_y').^2)*ds_y;
+
+                    if i == j
+                        msk_diagonals = logical(diag(ones(curve_n(j), 1)));
+                        A_local(msk_diagonals) = curr_x.w_prime(s_mesh_x).*curr_x.K_boundary_same_point(theta_mesh_y).*sqrt(curr_x.l_1_prime(theta_mesh_x).^2+curr_x.l_2_prime(theta_mesh_x).^2)*ds_x + 1/2;
+                    end
+
+                    A(start_idx(j):end_idx(j), start_idx(i):end_idx(i)) = A_local;
+
+                    curr_x = curr_x.next_curve;
+                end
+
+                curr_y = curr_y.next_curve;
+            end
+        end
+
+        function u_num = u_num(obj, x, y, curve_param, gr_phi)
+           curr = obj.first_curve;
+           u_num = 0;
+           for i = 1:obj.n_curves
+                u_num =  u_num + curr.u_num_curve(x, y, curve_param, gr_phi);
+                curr = curr.next_curve;
+            end
+        end
     end
 end
 
